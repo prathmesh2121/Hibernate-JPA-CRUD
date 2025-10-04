13486785d8332f9fc4ed7ad2ff811f17d128515d
Prathmesh Pawar <pawarprathmesh2121@gmail.com>
2025-10-04 22:30:06 +0530

    updated main file

--- Hibernate_JPA_CRUD/src/main/java/com/example/Hibernate_JPA_CRUD/HibernateJpaCrudApplication.java
+++ Hibernate_JPA_CRUD/src/main/java/com/example/Hibernate_JPA_CRUD/HibernateJpaCrudApplication.java
@@ -1,5 +1,7 @@
 package com.example.Hibernate_JPA_CRUD;
 
+import com.example.Hibernate_JPA_CRUD.dao.StudentDAO;
+import com.example.Hibernate_JPA_CRUD.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,11 +18,39 @@ public class HibernateJpaCrudApplication {
     // right after the application starts (after the Spring context is loaded).
 
     @Bean
-    public CommandLineRunner commandLineRunner(String[] args) {
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner -> {
-            System.out.println("Hello, Hibernate JPA CRUD Application!");
+            //createStudent(studentDAO);
+            createMultipleStudent(studentDAO);
+
         };
     }
 
+    private void createMultipleStudent(StudentDAO studentDAO)
+    {
+        System.out.println("Creating new student objects ...");
+
+        Student tempStudent1 = new Student("Mayur", "Kumbhar", "mayur.kumbhar@tcs.com");
+        Student tempStudent2 = new Student("Surbhi", "Marathe", "surbhi.marathe@forvia.com");
+        Student tempStudent3 = new Student("Tejas", "Ghate", "tejas.ghate@hsbc.com");
+        studentDAO.save(tempStudent1);
+        studentDAO.save(tempStudent2);
+        studentDAO.save(tempStudent3);
+        System.out.println(tempStudent1.getFirst_name()+" "+
+                           tempStudent2.getFirst_name()+" "+
+                           tempStudent3.getFirst_name()+" "+"Saved student objects ...");
+
+
+    }
+
+    private void createStudent(StudentDAO studentDAO)
+    {
+        System.out.println("Creating new student object ...");
+
+        Student tempStudent = new Student("Puja", "", "puja-borse@db.com");
+        studentDAO.save(tempStudent);
+
+        System.out.println(tempStudent.getFirst_name() + " : Student object saved ...");
+    }
 }
 
---
+++ Hibernate_JPA_CRUD/src/main/java/com/example/Hibernate_JPA_CRUD/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.example.Hibernate_JPA_CRUD.dao;
+
+import com.example.Hibernate_JPA_CRUD.entity.Student;
+
+public interface StudentDAO
+{
+    void save(Student theStudent);
+}
